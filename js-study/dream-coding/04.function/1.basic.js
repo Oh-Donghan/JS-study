6a15d05de9c43f8bab4bc5c5bdc68d106d19b4e1
Oh-Donghan <odonghan94@gmail.com>
2024-03-21 22:34:46 +0900

    js study

---
+++ js-study/dream-coding/04.function/1.basic.js
@@ -0,0 +1,17 @@
+function sum(a, b) {
+  return a + b;
+}
+const result = sum(1, 2);
+console.log(result);
+
+// 사용예제 2
+function fullName(firstName, lastName) {
+  return `${lastName} ${firstName}`;
+}
+let lastName = '김';
+let firstName = '지수';
+console.log(fullName(firstName, lastName));
+
+let lastName2 = '박';
+let firstName2 = '철수';
+console.log(fullName(firstName2, lastName2));
